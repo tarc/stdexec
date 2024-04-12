df1423f39ad4fbfba5227fc901dce11466f359fd
tarc <tarcisio.genaro@gmail.com>
2024-04-12 01:06:57 -0300

    Port to Conan 2

--- conanfile.py
+++ conanfile.py
@@ -1,24 +1,34 @@
-from conans import ConanFile, CMake, tools
+from conan import ConanFile
+from conan.tools.build import check_min_cppstd, can_run
+from conan.tools.cmake import CMake, cmake_layout, CMakeDeps, CMakeToolchain
+from conan.tools.files import copy
 import requests
 import re
 from os import path
 
 
 class P2300Recipe(ConanFile):
-    name = "P2300"
+    name = "p2300"
     description = "std::execution"
     author = "Michał Dominiak, Lewis Baker, Lee Howes, Kirk Shoop, Michael Garland, Eric Niebler, Bryce Adelstein Lelbach"
     topics = ("WG21", "concurrency")
     homepage = "https://github.com/NVIDIA/stdexec"
     url = "https://github.com/NVIDIA/stdexec"
     license = "Apache 2.0"
-    settings = "compiler"  # Header only - compiler only used for flags
-    tool_requires = "catch2/2.13.6"
-    exports_sources = "include/*"
-    generators = "cmake_find_package"
+    settings = "os", "arch", "compiler", "build_type"
+    exports_sources = "include/*", "test/*", "src/*", "CMakeLists.txt", "cmake/*", "examples/*"
+    no_copy_source = True
+    generators = "CMakeToolchain", "CMakeDeps"
+
+    @property
+    def _run_tests(self):
+        return not self.conf.get("tools.build:skip_test", default=True)
+
+    def requirements(self):
+        self.test_requires("catch2/2.13.7")
 
     def validate(self):
-        tools.check_min_cppstd(self,"20")
+        check_min_cppstd(self, "20")
 
     def set_version(self):
         # Get the version from the spec file
@@ -26,10 +36,27 @@ class P2300Recipe(ConanFile):
         rev = re.search(r"Revision: (\d+)", response.text).group(1).strip()
         self.version = f"0.{rev}.0"
 
+    def layout(self):
+        cmake_layout(self)
+
+    def build(self):
+        if self._run_tests:
+            cmake = CMake(self)
+            cmake.configure()
+            cmake.build()
+            if can_run(self):
+                cmake.test()
+
     def package(self):
-        self.copy("*.hpp")
+        copy(self, "*.hpp", self.source_folder, self.package_folder)
 
     def package_info(self):
+        self.cpp_info.bindirs = []
+        self.cpp_info.libdirs = []
+
         # Make sure to add the correct flags for gcc
         if self.settings.compiler == "gcc":
             self.cpp_info.cxxflags = ["-fcoroutines", "-Wno-non-template-friend"]
+
+    def package_id(self):
+        self.info.clear()
--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -1,17 +1,26 @@
 import os
 
-from conans import ConanFile, CMake, tools
+from conan import ConanFile
+from conan.tools.cmake import CMake, cmake_layout
+from conan.tools.build import can_run
+
 
 class P2300TestConan(ConanFile):
-    settings = "compiler"
-    generators = "cmake"
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "CMakeDeps", "CMakeToolchain"
+
+    def requirements(self):
+        self.requires(self.tested_reference_str)
 
     def build(self):
         cmake = CMake(self)
         cmake.configure()
         cmake.build()
 
+    def layout(self):
+        cmake_layout(self)
+
     def test(self):
-        if not tools.cross_building(self):
-            os.chdir("bin")
-            self.run(".{}test_p2300".format(os.sep))
+        if can_run(self):
+            cmd = os.path.join(self.cpp.build.bindir, "test_stdexec")
+            self.run(cmd, env="conanrun")
